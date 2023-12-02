5b9810c52ba78a215782d17ff596e6a56add36ce
riccardong <nguyenvinhat@delago.vn>
2023-12-03 00:04:58 +0700

    fix prisma

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
   reactStrictMode: true,
   swcMinify: true,
   images: {
