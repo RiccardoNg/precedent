3a0bb141733dbc999bcd7fd433530b8b14666b45
riccardong <nguyenvinhat@delago.vn>
2023-12-04 13:29:06 +0700

    register edit

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -22,3 +22,19 @@
 //     return NextResponse.json(user);
 //   }
 // }
+
+
+export async function GET() {
+  
+  const res = await fetch('https://body-mass-index-bmi-calculator.p.rapidapi.com/metric?weight=150&height=1.83', {
+    headers: {
+      // 'X-RapidAPI-Key': 'L5kiZdYz7Pmsh5VJpaWgy7hc8LSRp1YkeK9jsndroxpg3AQsBN',
+      'X-RapidAPI-Key': `${process.env.X_RapidAPI_Key}`,
+      'X-RapidAPI-Host': 'body-mass-index-bmi-calculator.p.rapidapi.com'
+    },
+  })
+  const data = await res.json()
+ 
+  return Response.json({ data })
+}
+
