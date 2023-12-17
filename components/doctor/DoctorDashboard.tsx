22d3503a7ee0968a4d7fb64a867b1188fa3911d6
riccardong <nguyenvinhat@delago.vn>
2023-12-17 10:04:13 +0700

    update caro and doctor

--- components/doctor/DoctorDashboard.tsx
+++ components/doctor/DoctorDashboard.tsx
@@ -5,11 +5,82 @@ export default function DoctorDashboard() {
 
     return (
         <div>
-            <button className="reset">
+            <h2 className="text-4xl font-extrabold dark:text-white">
                 Doctor Dashboard
+            </h2>
+            <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
                 My Patient
-                Health History
-            </button>
+            </p>
+            
+            <br/>
+                <div className="relative overflow-x-auto">
+                    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" className="px-6 py-3">
+                                    Ten benh nhan
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Can nang
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Category
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Huyet ap
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    Nguyen Minh Hai
+                                </th>
+                                <td className="px-6 py-4">
+                                    Silver
+                                </td>
+                                <td className="px-6 py-4">
+                                    Laptop
+                                </td>
+                                <td className="px-6 py-4">
+                                    $2999
+                                </td>
+                            </tr>
+                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    Nguyen Anh Tuan
+                                </th>
+                                <td className="px-6 py-4">
+                                    White
+                                </td>
+                                <td className="px-6 py-4">
+                                    Laptop PC
+                                </td>
+                                <td className="px-6 py-4">
+                                    $1999
+                                </td>
+                            </tr>
+                            <tr className="bg-white dark:bg-gray-800">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    Pham Thanh Thuy
+                                </th>
+                                <td className="px-6 py-4">
+                                    Black
+                                </td>
+                                <td className="px-6 py-4">
+                                    Accessories
+                                </td>
+                                <td className="px-6 py-4">
+                                    $99
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                
+                
+            
         </div>
     )
 }
