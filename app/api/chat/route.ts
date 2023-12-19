37e8c0e41873da0ea12d8f392a23a411eca3c66d
riccardong <nguyenvinhat@delago.vn>
2023-12-19 22:33:18 +0700

    openai preview

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -38,24 +38,72 @@
 
 
 
-// ====
-// ./app/api/chat/route.js
+// // ====
+// // ./app/api/chat/route.js
+// import OpenAI from 'openai';
+// import { OpenAIStream, StreamingTextResponse } from 'ai';
+
+// const openai = new OpenAI({
+//   apiKey: process.env.OPEN_AI,
+// });
+
+// export const runtime = 'edge';
+
+// export async function POST(req:Request) {
+//   const { messages } = await req.json();
+//   const response = await openai.chat.completions.create({
+//     model: 'gpt-3.5-turbo',
+//     stream: true,
+//     messages,
+//   });
+//   const stream = OpenAIStream(response);
+//   return new StreamingTextResponse(stream);
+// }
+
+
+//======
 import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
-
+ 
+// Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
   apiKey: process.env.OPEN_AI,
 });
-
+ 
+// IMPORTANT! Set the runtime to edge
 export const runtime = 'edge';
-
-export async function POST(req:Request) {
-  const { messages } = await req.json();
+ 
+export async function POST(req: Request) {
+  // 'data' contains the additional data that you have sent:
+  const { messages, data } = await req.json();
+ 
+  const initialMessages = messages.slice(0, -1);
+  const currentMessage = messages[messages.length - 1];
+ 
+  // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
+    model: 'gpt-4-vision-preview',
     stream: true,
-    messages,
+    max_tokens: 150,
+    messages: [
+      ...initialMessages,
+      {
+        ...currentMessage,
+        content: [
+          { type: 'text', text: currentMessage.content },
+ 
+          // forward the image information to OpenAI:
+          {
+            type: 'image_url',
+            image_url: data.imageUrl,
+          },
+        ],
+      },
+    ],
   });
+ 
+  // Convert the response into a friendly text-stream
   const stream = OpenAIStream(response);
+  // Respond with the stream
   return new StreamingTextResponse(stream);
 }
--- app/openai/page.tsx
+++ app/openai/page.tsx
@@ -1,30 +1,76 @@
-// ./app/page.js
+// // ./app/page.js
+// 'use client';
+
+// import { useChat } from 'ai/react';
+
+// export default function Chat() {
+//   const { messages, input, handleInputChange, handleSubmit } = useChat();
+
+//   return (
+//     <div className="w-full max-w-xl px-5 xl:px-0">
+//       <div className="relative float flex-col bg-clip-border rounded-xl bg-white text-gray-700 h-[calc(100vh-2rem)] w-full p-4 shadow-xl shadow-blue-gray-900/5">
+    
+  
+//         {messages.map(m => (
+//           <div key={m.id}>
+//             {m.role}: {m.content}
+//           </div>
+//         ))}
+
+//         <form onSubmit={handleSubmit}>
+//           <input
+//             value={input}
+//             placeholder="Say something..."
+//             onChange={handleInputChange}
+//           />
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }
+
+
 'use client';
-
+ 
 import { useChat } from 'ai/react';
-
+ 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
-
+ 
   return (
     <div className="w-full max-w-xl px-5 xl:px-0">
       <div className="relative float flex-col bg-clip-border rounded-xl bg-white text-gray-700 h-[calc(100vh-2rem)] w-full p-4 shadow-xl shadow-blue-gray-900/5">
     
-  
-        {messages.map(m => (
-          <div key={m.id}>
-            {m.role}: {m.content}
-          </div>
-        ))}
-
-        <form onSubmit={handleSubmit}>
-          <input
-            value={input}
-            placeholder="Say something..."
-            onChange={handleInputChange}
-          />
-        </form>
-      </div>
+        <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+          {messages.length > 0
+            ? messages.map(m => (
+                <div key={m.id} className="whitespace-pre-wrap">
+                  {m.role === 'user' ? 'User: ' : 'AI: '}
+                  {m.content}
+                </div>
+              ))
+            : null}
+    
+          <form
+            onSubmit={e => {
+              handleSubmit(e, {
+                data: {
+                  imageUrl:
+                    'http://cms.lucloimeme.com/uploads/KQ_ctm_fc2889e4a4.jpg?updated_at=2023-12-19T15:25:58.554Z',
+                },
+              });
+            }}
+          >
+            <input
+              className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+              value={input}
+              // value = "bạn hãy nhận diện các thông tin, chỉ số từ ảnh sau đây, sau đó bằng kiến thức của bạn hãy đưa nhận xét về chỉ số trên cho mục đích tham khảo"
+              placeholder="Bạn hãy nhận diện các thông tin, chỉ số từ ảnh sau đây, sau đó bằng kiến thức của bạn hãy đưa nhận xét về chỉ số trên cho mục đích tham khảo"
+              onChange={handleInputChange}
+            />
+          </form>
+        </div>
+        </div>
     </div>
   );
 }
